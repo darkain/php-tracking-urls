c090420144e0dd59c504ea43a3fb94d233aec1c6
Vince <git@darkain.com>
2017-05-02 12:11:29 -0700

    Updated to support new tracking number formats Added automated unit testing with Travis CI Removed hook for Altaform

--- php-tracking-urls.php
+++ php-tracking-urls.php
@@ -3,44 +3,55 @@
 function get_tracking_url($tracking_number) {
 	if (empty($tracking_number)) return false;
 
-	$php_tracking_urls = array(
-		array(
+	static $tracking_urls = [
+		//UPS - UNITED PARCEL SERVICE
+		[
 			'url'=>'http://wwwapps.ups.com/WebTracking/processInputRequest?TypeOfInquiryNumber=T&InquiryNumber1=',
-			'reg'=>'/\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/i'
-		),
-		array(
-			'url'=>'https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=',
-			'reg'=>'/\b((420 ?\d\d\d\d\d ?)?(91|94|01|03|04|70|23|13)\d\d ?\d\d\d\d ?\d\d\d\d ?\d\d\d\d ?\d\d\d\d( ?\d\d)?)\b/i'
-		),
-		array(
-			'url'=>'https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=',
-			'reg'=>'/\b((M|P[A-Z]?|D[C-Z]|LK|EA|V[A-Z]|R[A-Z]|CP|CJ|LC|LJ) ?\d\d\d ?\d\d\d ?\d\d\d ?[A-Z]?[A-Z]?)\b/i'
-		),
-		array(
-			'url'=>'http://www.fedex.com/Tracking?language=english&cntry_code=us&tracknumbers=',
-			'reg'=>'/\b((96\d\d\d\d\d ?\d\d\d\d|96\d\d|\d\d\d\d) ?\d\d\d\d ?\d\d\d\d( ?\d\d\d)?)\b/i'
-		),
-		array(
-			'url'=>'http://www.ontrac.com/trackres.asp?tracking_number=',
-			'reg'=>'/\b(C\d\d\d\d\d\d\d\d\d\d\d\d\d\d)\b/i'
-		),
-		array(
-			'url'=>'http://www.dhl.com/content/g0/en/express/tracking.shtml?brand=DHL&AWB=',
-			'reg'=>'/\b(\d\d\d\d ?\d\d\d\d ?\d\d)\b/i'
-		),
-	);
+			'reg'=>'/\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|T\d{3} ?\d{4} ?\d{3})\b/i'
+		],
 
-	foreach ($php_tracking_urls as $item) {
+		//USPS - UNITED STATES POSTAL SERVICE - FORMAT 1
+		[
+			'url'=>'https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=',
+			'reg'=>'/\b((420 ?\d{5} ?)?(91|92|93|94|01|03|04|70|23|13)\d{2} ?\d{4} ?\d{4} ?\d{4} ?\d{4}( ?\d{2})?)\b/i'
+		],
+
+		//USPS - UNITED STATES POSTAL SERVICE - FORMAT 2
+		[
+			'url'=>'https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=',
+			'reg'=>'/\b((M|P[A-Z]?|D[C-Z]|LK|E[A-C]|V[A-Z]|R[A-Z]|CP|CJ|LC|LJ) ?\d{3} ?\d{3} ?\d{3} ?[A-Z]?[A-Z]?)\b/i'
+		],
+
+		//USPS - UNITED STATES POSTAL SERVICE - FORMAT 3
+		[
+			'url'=>'https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=',
+			'reg'=>'/\b(82 ?\d{3} ?\d{3} ?\d{2})\b/i'
+		],
+
+		//FEDEX - FEDERAL EXPRESS
+		[
+			'url'=>'http://www.fedex.com/Tracking?language=english&cntry_code=us&tracknumbers=',
+			'reg'=>'/\b(((96\d\d|6\d)\d{3} ?\d{4}|96\d{2}|\d{4}) ?\d{4} ?\d{4}( ?\d{3})?)\b/i'
+		],
+
+		//ONTRAC
+		[
+			'url'=>'http://www.ontrac.com/trackres.asp?tracking_number=',
+			'reg'=>'/\b(C\d{14})\b/i'
+		],
+
+		//DHL
+		[
+			'url'=>'http://www.dhl.com/content/g0/en/express/tracking.shtml?brand=DHL&AWB=',
+			'reg'=>'/\b(\d{4}[- ]?\d{4}[- ]?\d{2}|\d{3}[- ]?\d{8}|[A-Z]{3}\d{7})\b/i'
+		],
+	];
+
+	foreach ($tracking_urls as $item) {
 		$match = array();
 		preg_match($item['reg'], $tracking_number, $match);
 		if (count($match)) return $item['url'] . $match[0];
 	}
 
 	return false;
-}
-
-
-//Attach this to AltaForm if available
-if (!empty($afurl)  &&  is_a($afurl, 'af_url')) {
-	$afurl->tracking = 'get_tracking_url';
 }
---
+++ test.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once('php-tracking-urls.php');
+
+$errors = false;
+
+$numbers = [
+	//TEST UPS
+	'1Z12345E1512345676'			=> 'ups.com',
+	'1Z12345E0205271688'			=> 'ups.com',
+	'1Z12345E6605272234'			=> 'ups.com',
+	'1Z12345E0305271640'			=> 'ups.com',
+	'1Z12345E0393657226'			=> 'ups.com',
+	'1Z12345E1305277940'			=> 'ups.com',
+	'1Z12345E6205277936'			=> 'ups.com',
+	'1Z12345E1505270452'			=> 'ups.com',
+	'1Z648616E192760718'			=> 'ups.com',
+	'1ZWX0692YP40636269'			=> 'ups.com',
+	'T9999999999'					=> 'ups.com',
+
+	//TEST FEDEX
+	'9999 9999 9999'				=> 'fedex.com',
+	'9999 9999 9999 999'			=> 'fedex.com',
+	'999999999999'					=> 'fedex.com',
+	'999999999999999'				=> 'fedex.com',
+	'661377569221'					=> 'fedex.com',
+	'624893691092'					=> 'fedex.com',
+	'61299995669352455464'			=> 'fedex.com',
+	'61299995669151880177'			=> 'fedex.com',
+
+	//TEST USPS
+	'9400 1000 0000 0000 0000 00'	=> 'usps.com',
+	'9205 5000 0000 0000 0000 00'	=> 'usps.com',
+	'9407 3000 0000 0000 0000 00'	=> 'usps.com',
+	'9303 3000 0000 0000 0000 00'	=> 'usps.com',
+	'82 000 000 00'					=> 'usps.com',
+	'EC 000 000 000 US'				=> 'usps.com',
+	'9270 1000 0000 0000 0000 00'	=> 'usps.com',
+	'EA 000 000 000 US'				=> 'usps.com',
+	'CP 000 000 000 US'				=> 'usps.com',
+	'9208 8000 0000 0000 0000 00'	=> 'usps.com',
+	'9202 1000 0000 0000 0000 00'	=> 'usps.com',
+	'9400100000000000000000'		=> 'usps.com',
+	'9205500000000000000000'		=> 'usps.com',
+	'9407300000000000000000'		=> 'usps.com',
+	'9303300000000000000000'		=> 'usps.com',
+	'8200000000'					=> 'usps.com',
+	'EC000000000US'					=> 'usps.com',
+	'9270100000000000000000'		=> 'usps.com',
+	'EA000000000US'					=> 'usps.com',
+	'CP000000000US'					=> 'usps.com',
+	'9208800000000000000000'		=> 'usps.com',
+	'9202100000000000000000'		=> 'usps.com',
+
+	//TEST ONTRAC
+	'C00000000000000'				=> 'ontrac.com',
+	'C99999999999999'				=> 'ontrac.com',
+
+	//TEST DHL
+	'125-12345678'					=> 'dhl.com',
+	'125 12345678'					=> 'dhl.com',
+	'12512345678'					=> 'dhl.com',
+	'SEA1234567'					=> 'dhl.com',
+	'LAX1234567'					=> 'dhl.com',
+];
+
+
+
+foreach ($numbers as $number => $service) {
+	echo 'TESTING: ' . $number;
+	if ($url = get_tracking_url($number)) {
+		echo "\n" . $url;
+		if (!strpos($url, $service)) {
+			echo "\n  ---  FAILED  ---  WRONG SERVICE DETECTED  ---";
+			$errors = true;
+		}
+	} else {
+		echo "\n  ---  FAILED  ---  NO SERVICE FOUND  ---";
+		$errors = true;
+	}
+	echo "\n\n";
+}
+
+
+if ($errors) {
+	echo " --- ERROR FOUND  ---  TEST FAILED  ---\n\n";
+	exit(1);
+} else {
+	echo " !!! ALL GOOD !!!\n\n";
+}
